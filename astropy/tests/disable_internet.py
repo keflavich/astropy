a533f9d29bc1bc49a7e3b2be9c7a93fddf84848c
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-02-05 21:26:58 +0100

    Use a decorator to simplify code

--- astropy/tests/disable_internet.py
+++ astropy/tests/disable_internet.py
@@ -9,6 +9,24 @@ socket_create_connection = socket.create_connection
 socket_bind = socket.socket.bind
 socket_connect = socket.socket.connect
 
+
+# ::1 is apparently another valid name for localhost?
+# it is returned by getaddrinfo when that function is given localhost
+
+
+def check_internet_off(original_function):
+    def new_function(*args, **kwargs):
+        if isinstance(args[0], socket.socket):
+            host = args[1][0]
+        else:
+            host = args[0][0]
+        if '127.0.0.1' in host or 'localhost' in host or '::1' in host:
+            return original_function(*args, **kwargs)
+        else:
+            raise IOError("An attempt was made to connect to the internet")
+    return new_function
+
+
 def turn_off_internet(verbose=False):
     """
     Disable internet access via python by preventing connections from being
@@ -19,39 +37,11 @@ def turn_off_internet(verbose=False):
     __tracebackhide__ = True
     if verbose:
         print("Internet access disabled")
-
-    # ::1 is apparently another valid name for localhost?
-    # it is returned by getaddrinfo when that function is given localhost
-
-    def blocker_creator(return_fn, isclass=False):
-        __tracebackhide__ = True
-        def blocker(*args, **kwargs):
-            if isclass:
-                address = args[1]
-            else:
-                address = args[0]
-            host,port = address[:2]
-            if not ('127.0.0.1' in host or
-                    'localhost' in host or
-                    '::1' in host): 
-                raise IOError("An attempt was made to connect to the internet")
-            else:
-                return return_fn(*args, **kwargs)
-        return blocker
-
-    socket_functions = {'bind':socket_bind,
-                        'connect':socket_connect,
-                        'create_connection':socket_create_connection}
-    parents = {'bind':socket.socket,
-               'connect':socket.socket,
-               'create_connection':socket}
-    guard_functions = dict([(fnc,blocker_creator(oldfnc, hasattr(parents[fnc],'bind')))
-                            for fnc,oldfnc in socket_functions.iteritems()])
-
-    for fnc, guard in guard_functions.iteritems():
-        setattr(parents[fnc], fnc, guard)
-
+    socket.create_connection = check_internet_off(socket_create_connection)
+    socket.socket.bind = check_internet_off(socket_bind)
+    socket.socket.connect = check_internet_off(socket_connect)
     return socket
+
 
 def turn_on_internet(verbose=False):
     """
@@ -59,8 +49,7 @@ def turn_on_internet(verbose=False):
     """
     if verbose:
         print("Internet access enabled")
-    #setattr(socket, 'socket', socket_original)
-    setattr(socket, 'create_connection', socket_create_connection)
-    setattr(socket.socket, 'bind', socket_bind)
-    setattr(socket.socket, 'connect', socket_connect)
+    socket.create_connection = socket_create_connection
+    socket.socket.bind = socket_bind
+    socket.socket.connect = socket_connect
     return socket
